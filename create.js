48c80904444150aca341d26491996773ecf83556
David Cabeen <mediatecnicadavidcabeen@gmail.com>
2025-05-02 15:34:58 -0500

    more note fixes

--- create.js
+++ create.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function handleSubmit() {
+    const button = document.querySelector('button'),
+    noteBox = document.getElementById('note');
+
+    button.addEventListener('click', () => {
         const newAcc = {
             name : document.getElementById('name').value,
-            note : document.getElementById('note').value,
+            note : noteBox.value,
             currency : document.getElementById('currency').value,
             balance : document.getElementById('balance').value
         };
         localStorage.setItem('account', JSON.stringify(newAcc));
-    };
+    });
+
+    noteBox.addEventListener('input', () => {
+        const label = noteBox.nextElementSibling.style;
+        if (noteBox.value) {
+            label.top = 0;
+            label.left = '5%';
+            label.fontSize = '90%';
+        } else {
+            label.top = '50%';
+            label.left = '2%';
+            label.fontSize = '100%';
+        }
+    });
 });
