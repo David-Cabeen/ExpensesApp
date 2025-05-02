a11199a4aabc1377308ad240d409bbed33665b6d
David Cabeen <mediatecnicadavidcabeen@gmail.com>
2025-05-02 15:29:51 -0500

    note fix

--- create.js
+++ create.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function handleSubmit() {
         const newAcc = {
             name : document.getElementById('name').value,
-            description : document.getElementById('description').value,
+            note : document.getElementById('note').value,
             currency : document.getElementById('currency').value,
             balance : document.getElementById('balance').value
         };
