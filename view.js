63a806682b22ec347e6fef49b882cbb8e71f6bfd
David Cabeen <mediatecnicadavidcabeen@gmail.com>
2025-05-02 15:04:51 -0500

    test accounts

---
+++ view.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let accounts = localStorage.getItem('accounts');
+    const accountsParent = document.querySelector('.account-view')
+    if (!accounts) {
+        accountsParent.textContent = 'No accounts created \n Make one!';
+    } else {
+        const div = document.createElement('div'),
+        accountName = document.createElement('h2'),
+        currency = document.createElement('h3'),
+        balance = document.createElement('h3');
+        accountName.textContent = localStorage.getItem('accounts');
+        div.append(accountName, document.createElement('ion-icon').setAttribute('name', 'create-outline'), document.createElement('hr'), currency.classList.add('currency'), balance.classList.add('balance'));
+        accountsParent.appendChild(div);
+    }
+});
