ec1066eb7f0a986f5c7a7a6e364947e1ecc601c1
David Cabeen <mediatecnicadavidcabeen@gmail.com>
2025-05-05 20:58:36 -0500

    vru

--- edit.js
+++ edit.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const accountToBeEdited = urlParams.get('account');
-    accounts = JSON.parse(localStorage.getItem('accounts'))
+    const accounts = JSON.parse(localStorage.getItem('accounts'))
+    
     accounts.forEach(account => {
         if (account.name == accountToBeEdited){
             const h2 = document.createElement('h2');
