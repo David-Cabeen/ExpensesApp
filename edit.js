fddad8f59fa1b1c2d0ddf02bd9cdf84201009090
David Cabeen <mediatecnicadavidcabeen@gmail.com>
2025-05-05 20:57:51 -0500

    s

--- edit.js
+++ edit.js
@@ -4,7 +4,8 @@ window.addEventListener('DOMContentLoaded', () => {
     accounts = JSON.parse(localStorage.getItem('accounts'))
     accounts.forEach(account => {
         if (account.name == accountToBeEdited){
-            document.write("<div class=\"name\">\r\n            <input id=\"name\" name=\"name\" spellcheck=\"false\" required type=\"text\"> \r\n            <label for=\"name\">Name<\/label>\r\n        <\/div>\r\n        <hr>\r\n        <div class=\"note\">\r\n            <textarea name=\"description\" id=\"note\"><\/textarea> <label for=\"description\">Description<\/label>\r\n        <\/div>\r\n        <input type=\"text\" name=\"currency\" id=\"currency\">\r\n        <input type=\"number\" name=\"balance\" id=\"balance\" step=\"any\" min=\"1\">\r\n        <button class=\"acc-create\">Create account<\/button>");
+            const h2 = document.createElement('h2');
+            h2.textContent = accountToBeEdited;
         }
     });
 });
